685bfa71aafb25cb9e56d40cda7a602bec056bd5
Laali Nembot <laalinembot@gmail.com>
2026-06-24 02:04:40 +0000

    Worked on command for command using Betting API

--- main.py
+++ main.py
@@ -1,25 +1,92 @@
 # import requests for APIs
 import requests
 
-response = requests.get(
-    'https://api.balldontlie.io/fifa/worldcup/v1/teams',
-    headers={'Authorization': 'c6a27bdd-ca76-4875-9b5b-b765a4e65ffa'}
-)
-
-teams = response.json()['data']
-for team in teams:
-    team_name = team['name']
-    print(team_name)
-
-#second API
 response_2 = requests.get("https://api.the-odds-api.com/v4/sports/soccer_fifa_world_cup/odds?regions=eu&markets=h2h,spreads,totals&oddsFormat=decimal&apiKey=3527d7d205521b10d613ced02cdface2")
-betting_data = response_2.json()
+betting_json = response_2.json()
+
+world_cup_teams = [
+    "Argentina",
+    "Australia",
+    "Austria",
+    "Belgium",
+    "Bosnia and Herzegovina",
+    "Brazil",
+    "Canada",
+    "Cape Verde",
+    "Colombia",
+    "Croatia",
+    "Curacao",
+    "Czechia",
+    "DR Congo",
+    "Ecuador",
+    "Egypt",
+    "England",
+    "France",
+    "Germany",
+    "Ghana",
+    "Haiti",
+    "Iran",
+    "Iraq",
+    "Ivory Coast",
+    "Japan",
+    "Jordan",
+    "Mexico",
+    "Morocco",
+    "Netherlands",
+    "New Zealand",
+    "Norway",
+    "Panama",
+    "Paraguay",
+    "Portugal",
+    "Qatar",
+    "Saudi Arabia",
+    "Scotland",
+    "Senegal",
+    "South Africa",
+    "South Korea",
+    "Spain",
+    "Sweden",
+    "Switzerland",
+    "Tunisia",
+    "Turkey",
+    "United States",
+    "Uruguay",
+    "Uzbekistan",
+    "Algeria"
+]
+
+user_team = str(input("Hello, please choose a team to begin: "))
+
+
+if user_team in world_cup_teams:
+    print("Great let's fetch this teams next opponent where you can check out the teams moneyline odds for the next game via Pinnacle")
+    for game in betting_json:
+        if user_team == game['home_team'] or user_team == game['away_team']:
+            for bet_platform in game['bookmakers']:
+                if bet_platform['title'] == 'Pinnacle':
+                    for outcome in bet_platform['markets']:
+                        if outcome['key'] == 'h2h':
+                            h2h_dict = {}
+                            for moneyline in outcome['outcomes']:
+                                h2h_dict[moneyline['name']] = moneyline['price']
+                            list_of_odds = []
+                            for key, value in h2h_dict.items():
+                                list_of_odds.append(f"{key} {value}")
+                            final_format = " | ".join(list_of_odds)
+                            print(f"Moneyline for next match: {final_format}")
+            
+    
+
 
 
 # use http requests to get access to the JSON files from the odds/betting API and the players data API
 
 
-# opening message - welcome user to app and ask them to enter a team name (condition for whether team is in world cup or not) or 
+
+# opening message - welcome user to app and ask them to enter a team name (condition for whether team is in world cup or not)
+#  or 
+
+    
 
 
 # move on to user being able to select players (1), schedule (2) and betting odds for most recent/latest game (3)
